a9423608aab0a0aaef453b1bee5dad6839736580
Bruno Higino de Souza <brunohigino06@gmail.com>
2023-05-02 14:25:51 -0300

    2023.05.02

---
+++ aws-vpc_done/examples/main.tf
@@ -0,0 +1,11 @@
+module "vpc" {
+  source = "..\\"
+
+  cidr_block = ["192.168.0.0/16", "10.0.0.0/16", "172.0.0.0/16"]
+  instance_tenancy = ["default", "default", "default"]
+  vpc-tags = {
+    Environment = "production"
+    Owner = "Alice"
+  }
+  name = ["vpc1", "vpc2", "vpc3"]
+}
--- aws-vpc_done/examples/provider.tf
+++ aws-vpc_done/examples/provider.tf
@@ -1,5 +1,9 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket = "mybucket2023050211111"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
